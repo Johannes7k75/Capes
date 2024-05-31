980ada1562cf354a6adc95d310a053b99550fa70
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-06-01 00:59:31 +0200

    Update CapeConfig.kt

--- common/src/main/kotlin/me/cael/capes/CapeConfig.kt
+++ common/src/main/kotlin/me/cael/capes/CapeConfig.kt
@@ -13,6 +13,7 @@ class CapeConfig {
     var enableMinecraftCapesMod = false
     var enableCosmetica = false
     var enableCloaksPlus = false
+    var enableJospace = false
     var enableElytraTexture = true
 
     fun save() {
